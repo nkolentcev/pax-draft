2b99457ccaebca955e2280d8d06ba9f45ffb7c54
Nikolay Kolentcev <nkolentcev@gmail.com>
2023-03-06 02:37:59 +0300

    переезд

--- srv/go.mod
+++ srv/go.mod
@@ -5,4 +5,5 @@ go 1.20
 require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
 )
--- srv/repository.go
+++ srv/repository.go
@@ -1,10 +1,21 @@
 package main
 
+import "database/sql"
+
 type Storage interface {
 	CreateUser(*User) error
-	UpdateUser(*User) error
+	//UpdateUser(*User) error - TODO обновление схемы пользователя, прав доступа
 	GetUserByPin(int) (*User, error)
-	CreateBoardingPass(*BPass) error
-	UpdateBoardingPass(*BPass) error
-	GetBoardingPassByBooking(string) (*BPass, error)
+	CreateBoardingPass(*BPass) error //создание проверка уже существующего
+	UpdateBoardingPass(*BPass) error //ОСНОВНАЯ ЛОГИКА
+	//GetBoardingPassByBooking(string) (*BPass, error) //TODO показать текущее состояние
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full"
+
 }
