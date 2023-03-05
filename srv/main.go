8ecb8ad21131fd34e17cc6dbf7d2baf589b295c2
Nikolay Kolentcev <nkolentcev@gmail.com>
2023-03-05 16:30:45 +0300

    update 20910

--- srv/main.go
+++ srv/main.go
@@ -4,4 +4,6 @@ import "log"
 
 func main() {
 	log.Println("running...")
+	service := NewAPIService(":8000")
+	service.Run()
 }
--- srv/service.go
+++ srv/service.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type APIService struct {
 	listenAddr string
@@ -12,12 +19,50 @@ func NewAPIService(listenAddr string) *APIService {
 	}
 }
 
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
 func (s *APIService) handleUser(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	if r.Method == "GET" {
+		return s.handleGetUser(w, r)
+	}
+	if r.Method == "PUT" {
+		return s.handleCreateUser(w, r)
+	}
+	if r.Method == "PATCH" {
+		return s.handleUpdateUser(w, r)
+	}
+	return fmt.Errorf("method undefined")
 }
 
 func (s *APIService) handleBoardingPass(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	if r.Method == "GET" {
+		return s.handleCheckBoardingPass(w, r)
+	}
+	if r.Method == "PUT" {
+		return s.handleCreateBoardingPass(w, r)
+	}
+	if r.Method == "PATCH" {
+		return s.handleUpdateBoardingPass(w, r)
+	}
+	return fmt.Errorf("method undefined")
 }
 
 // ПОЛЬЗОВАТЕЛИ
@@ -44,4 +89,14 @@ func (s *APIService) handleCreateBoardingPass(w http.ResponseWriter, r *http.Req
 
 func (s *APIService) handleUpdateBoardingPass(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+
+// / RUNNER
+func (s *APIService) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/user", makeHTTPHandlerFunc(s.handleUser))
+	router.HandleFunc("/bpas", makeHTTPHandlerFunc(s.handleBoardingPass))
+	log.Print("api server runinig on port: ", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
+
 }
